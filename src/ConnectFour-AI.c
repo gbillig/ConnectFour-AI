cedeba61ff186fc452e312e6e9fe76a02f0c06fc
gbillig <gleb.billig@gmail.com>
2015-02-01 13:36:39 -0500

    Initial source commit

---
+++ src/ConnectFour-AI.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ConnectFour-AI.c
+ Author      : Gleb Billig
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello"); /* prints  */
+	return EXIT_SUCCESS;
+}
