d7991fbfd117e9813a679766a12b7483a9da9386
gbillig <gleb.billig@gmail.com>
2015-04-28 13:02:58 -0400

    Added diagonal line-search to heuristic3

--- src/heurisitic3.c
+++ src/heurisitic3.c
@@ -35,8 +35,8 @@ int calc_lines3(gridType grid) {
 
 	searchVertical(grid);
 	searchHorizontal(grid);
-	//searchDiag1(grid);
-	//searchDiag2(grid);
+	searchDiag1(grid);
+	searchDiag2(grid);
 
 
 	int value = 0;
@@ -54,13 +54,14 @@ int calc_lines3(gridType grid) {
 		value -= lineCounter[1][0]*twoLineValue + lineCounter[1][1]*threeLineValue;
 	}
 
-
+	/*
 	printf("%d 2-line found for P1\n", lineCounter[P1][0]);
 	printf("%d 3-line found for P1\n", lineCounter[P1][1]);
 	printf("%d 4-line found for P1\n", lineCounter[P1][2]);
 	printf("%d 2-line found for P2\n", lineCounter[P2][0]);
 	printf("%d 3-line found for P2\n", lineCounter[P2][1]);
 	printf("%d 4-line found for P2\n", lineCounter[P2][2]);
+	*/
 
 
 	//displayGrid(grid);
@@ -100,8 +101,10 @@ void searchVertical(gridType grid) {
 
 			if (local_valid && local_count > 1) {
 				lineCounter[local_player][local_count-2]++;
+				/*
 				printf("%d-line (vert)  found between (%d,%d) and (%d,%d)\n", local_count,
 								i, 5-bottom_bound, i, 5-top_bound);
+				*/
 			}
 
 			bottom_bound--;
@@ -140,13 +143,125 @@ void searchHorizontal(gridType grid) {
 
 			if (local_valid && local_count > 1) {
 				lineCounter[local_player][local_count-2]++;
+				/*
 				printf("%d-line (hor)   found between (%d,%d) and (%d,%d)\n", local_count,
 								left_bound, 5-j, right_bound, 5-j);
+				*/
 			}
 
 			left_bound++;
 			right_bound++;
 
+		}
+	}
+}
+
+
+
+void searchDiag1(gridType grid) {
+	int top_bound, bottom_bound, left_bound, right_bound;
+	int i,j,k;
+	int local_count, local_player, local_valid;
+
+	for (k=0; k<6; k++) {
+
+		if (k<2) {
+			left_bound = 0;
+			right_bound = 3;
+			bottom_bound = k + 3;
+			top_bound = k;
+		} else {
+			left_bound = k - 2;
+			right_bound = k + 1;
+			bottom_bound = 5;
+			top_bound = 2;
+		}
+
+		while (top_bound >= 0 && right_bound <= 6) {
+			local_player = -1;
+			local_count = 0;
+			local_valid = 1;
+			for (i=left_bound, j=bottom_bound; i<=right_bound; i++, j--) {
+				if (local_valid) {
+					if (grid[i][j] != EMPTY) {
+						if (local_player == -1) {
+							local_player = grid[i][j];
+							local_count++;
+						} else if (grid[i][j] == local_player) {
+							local_count++;
+						} else {
+							local_valid = 0;
+						}
+					}
+				}
+			}
+
+			if (local_valid && local_count > 1) {
+				lineCounter[local_player][local_count-2]++;
+				/*
+				printf("%d-line (diag1) found between (%d,%d) and (%d,%d)\n", local_count,
+								left_bound, 5-bottom_bound, right_bound, 5-top_bound);
+				*/
+			}
+
+			left_bound++;
+			right_bound++;
+			top_bound--;
+			bottom_bound--;
+		}
+	}
+}
+
+void searchDiag2(gridType grid) {
+	int top_bound, bottom_bound, left_bound, right_bound;
+	int i,j,k;
+	int local_count, local_player, local_valid;
+
+	for (k=0; k<6; k++) {
+
+		if (k<2) {
+			right_bound = 6;
+			left_bound = 3;
+			bottom_bound = k + 3;
+			top_bound = k;
+		} else {
+			right_bound = 8 - k;
+			left_bound = 5 - k;
+			bottom_bound = 5;
+			top_bound = 2;
+		}
+
+		while (top_bound >= 0 && left_bound >= 0) {
+			local_player = -1;
+			local_count = 0;
+			local_valid = 1;
+			for (i=right_bound, j=bottom_bound; i>=left_bound; i--, j--) {
+				if (local_valid) {
+					if (grid[i][j] != EMPTY) {
+						if (local_player == -1) {
+							local_player = grid[i][j];
+							local_count++;
+						} else if (grid[i][j] == local_player) {
+							local_count++;
+						} else {
+							local_valid = 0;
+						}
+					}
+				}
+			}
+
+			if (local_valid && local_count > 1) {
+				lineCounter[local_player][local_count-2]++;
+				/*
+				printf("%d-line (diag2) found between (%d,%d) and (%d,%d)\n", local_count,
+								left_bound, 5-top_bound, right_bound, 5-bottom_bound);
+				 */
+			}
+
+			left_bound--;
+			right_bound--;
+			top_bound--;
+			bottom_bound--;
 		}
 	}
 }
